7b836cf3fbd45bd57b4fadd4342e3de333b8e6c7
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 18:48:42 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def destroy_all
+    @@all.clear
+  end
+
+  def create(name)
+    instance = self.new(name)
+    save
+  end
+end
