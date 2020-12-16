0201a729de0628cda37a2267c2cb8386ebcb64b4
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 20:01:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -74,6 +74,16 @@ class MusicLibraryController
   end
 
   def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort do |a, b|
+        a.name <=> b.name
+      end.each_with_index(1) do |song, index|
+        puts "#{index}. #{song.name} - #{song.artist.name}"
+      end
+    end
   end
 
   def play_song
