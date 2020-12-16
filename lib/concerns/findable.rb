c9a580442906e148b25c6065bf242de09d114225
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 19:20:16 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,17 @@
 module Concerns::Findable
   def find_by_name(name)
-
+    self.class.all.detect do |song|
+      song.name === name
+    end
   end
+end
+
+def self.find_by_name(name)
+  @@all.detect do |song|
+    song.name === name
+  end
+end
+
+def self.find_or_create_by_name(name)
+  find_by_name(name) || create(name)
 end
