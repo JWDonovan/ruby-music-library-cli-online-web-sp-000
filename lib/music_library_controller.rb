ac627f0c9c32234fcde9883a97123d5a5d456f63
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 20:03:38 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -87,5 +87,11 @@ class MusicLibraryController
   end
 
   def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i
+
+    if (1..Song.all.length).include?(input)
+
+    puts "Playing #{song.name} by #{song.artist.name}" if song
   end
 end
